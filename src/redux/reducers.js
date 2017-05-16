521bf5ef562dedba282ee10a14ccd8e313fda213
Nikordaris <nikordaris@gmail.com>
2017-05-16 09:12:49 +0100

    Added project structure

---
+++ src/redux/reducers.js
@@ -0,0 +1,2 @@
+import { createActionAsync, createReducerAsync } from 'redux-act-async';
+import { createAction } from 'redux-act';
---
+++ stories/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { storiesOf, addDecorator } from '@kadira/storybook';
+
+import reducers from '../src/redux/reducers';
+import middleware from '../src/redux/middleware';
+
+const store = createStore(reducers, applyMiddleware(middleware));
+
+addDecorator((getStory) => (
+  <Provider store={store}>
+    {getStory()}
+  </Provider>
+));
+
+storiesOf('ForceGraph', module)
+  .add('to Storybook', () => (
+    <p>TODO</p>
+  ));
